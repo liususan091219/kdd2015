fdd1955af0b66e29859b9f53701c3e66e67180c5
liususan091219 <liususan091219@gmail.com>
2015-07-02 00:34:52 -0500

    comment on node.m

--- Library/node.m
+++ Library/node.m
@@ -9,14 +9,14 @@
 % =======================================================================
 classdef node < handle
    properties     
-   children = {};
-   parent = [];
-   twmati=[];
-   twmatparent = [];
-   topici = [];
-   alpha0 = [];
-   pz = [];
-   name = [];
+   children = {}; % children of the node
+   parent = []; % parent of the node
+   twmati=[]; % a V-dimensional vector, the w-th dimension corresponding to p(w|i), where i is the index of this node among the node's parent's children
+   twmatparent = []; % a k x V matrix, where twmatparent(z, w) is equal to p(w|z), and z is the z-th topic/node among this node's children
+   topici = []; % the top-10 word w according to p(w|i), sorted in descending order.
+   alpha0 = []; % input parameter of EXP
+   pz = []; % topic distribution p(i) of this node
+   name = []; % name of this node
    voc_V_map = []; % when the tree goes down, some of the nodes has vocabulary smaller than V, so needs a map to the original vocabulary
    voc_p_map = []; % vocabulary map to the node's parent
    end
